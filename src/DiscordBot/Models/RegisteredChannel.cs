07ce372e94488707904cacf631e6293304e5dc27
Jordan C <nykezgaming@live.com>
2020-11-27 01:43:08 -0500

    add ctors to models

--- src/DiscordBot/Models/RegisteredChannel.cs
+++ src/DiscordBot/Models/RegisteredChannel.cs
@@ -7,6 +7,14 @@ namespace DiscordBot.Models
 {
     public class RegisteredChannel
     {
+        public RegisteredChannel() { }
+
+        public RegisteredChannel(ulong channelID, ulong guildID)
+        {
+            ChannelId = channelID;
+            GuildId = guildID;
+        }
+
         [Key]
         [Required]
         public ulong ChannelId { get; set; }
