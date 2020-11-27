4eb8c8e6e2b11dba82e1f72e1569df433fd340f8
Jordan C <nykezgaming@live.com>
2020-11-27 01:41:38 -0500

    add channel model

---
+++ src/DiscordBot/Models/RegisteredChannel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DiscordBot.Models
+{
+    public class RegisteredChannel
+    {
+        [Key]
+        [Required]
+        public ulong ChannelId { get; set; }
+        [Required]
+        public ulong GuildId { get; set; }
+    }
+
+}
