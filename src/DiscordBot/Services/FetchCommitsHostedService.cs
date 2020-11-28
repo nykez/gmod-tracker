e2a145ddf8162dd41097099f19564b0ddec3a54f
Jordan C <nykezgaming@live.com>
2020-11-28 17:05:20 -0500

    add IHostedService: FetchCommitsHostedService

---
+++ src/DiscordBot/Services/FetchCommitsHostedService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+
+namespace DiscordBot.Services
+{
+    public class FetchCommitsHostedService: IHostedService
+    {
+        private readonly ILogger<FetchCommitsHostedService> _logger;
+        private readonly IBroadcastService _broadcastService;
+
+        public FetchCommitsHostedService(ILogger<FetchCommitsHostedService> logger, IBroadcastService broadcastService)
+        {
+            _logger = logger;
+            _broadcastService = broadcastService;
+        }
+
+        Task IHostedService.StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IHostedService.StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
