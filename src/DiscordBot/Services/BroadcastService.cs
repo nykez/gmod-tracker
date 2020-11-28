4d40e5a22acd2537564a62b7fa79d2a4900e7961
Jordan C <nykezgaming@live.com>
2020-11-28 16:59:38 -0500

    updated IBroadCstService

--- src/DiscordBot/Services/BroadcastService.cs
+++ src/DiscordBot/Services/BroadcastService.cs
@@ -15,8 +15,13 @@ namespace DiscordBot.Services
 {
     public interface IBroadcastService
     {
-        Task BroadcastAllGuilds();
+     
+        /// <param name="commits"></param>
+        /// <returns></returns>
+        Task BroadcastAllGuilds(List<Commit> commits);
 
+
+        /// <returns></returns>
         Task CheckCommits();
     }
 
@@ -30,6 +35,13 @@ namespace DiscordBot.Services
 
         public int LastCommitId { get; set; } = 0;
 
+        /// <summary>
+        /// Creeates BroadCastServer
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="client"></param>
+        /// <param name="httpClient"></param>
+        /// <param name="context"></param>
         public BroadcastService(ILogger<BroadcastService> logger, DiscordSocketClient client, HttpClient httpClient, BotContext context)
         {
             _logger = logger;
@@ -39,10 +51,17 @@ namespace DiscordBot.Services
 
         }
 
-        public async Task BroadcastAllGuilds()
+        /// <summary>
+        /// Broadcast commits to all current guilds
+        /// </summary>
+        /// <param name="commits"></param>
+        /// <returns></returns>
+
+        public async Task BroadcastAllGuilds(List<Commit> commits)
         {
             // get all channels in database
             var channels = _context.Channels.ToListAsync().Result;
+            bool hasChanges = true;
 
             // loop all channels
             foreach (var item in channels)
@@ -50,7 +69,7 @@ namespace DiscordBot.Services
                 // check if channel exists
                 // if it doesn't then remove it from database
 
-                var channel = _client.GetChannel(item.ChannelId) as IChannel;
+                var channel = _client.GetChannel(item.ChannelId) as IMessageChannel;
                 if ( channel == null )
                 {
                     // are we still in the guild even?
@@ -59,6 +78,10 @@ namespace DiscordBot.Services
                         // nope we're not. Delete everything
                         // This should already be handled when the bot leaves a server
                         // but we will check it here too?
+
+                        _context.Remove(item);
+
+                        hasChanges = true;
 
                         continue;
                     }
@@ -75,7 +98,17 @@ namespace DiscordBot.Services
                     {
                         _logger.Log(LogLevel.Critical, ex.ToString());
                     }
+
+                    continue;
                 }
+
+                // actually message the channel
+                await channel.SendMessageAsync($"A new commit!");
+            }
+
+            if (hasChanges)
+            {
+                await _context.SaveChangesAsync();
             }
         }
 
@@ -85,7 +118,28 @@ namespace DiscordBot.Services
 
         public async Task CheckCommits()
         {
-            using var result = _httpClient.GetFromJsonAsync<Result>("/r/Garrys Mod?format=json");
+            // get the commit log
+            var result = await _httpClient.GetFromJsonAsync<Result>("/r/Garrys Mod?format=json");
+
+            // make sure commit log is actually valid? dead website?
+            // todo: add http status code
+            if (result != null)
+            {
+                var lastestCommit = result.results.First();
+
+                if ( Convert.ToInt32(lastestCommit.Changeset) != LastCommitId )
+                {
+                    // we have changes
+                    // let's broadcast them and do some updating
+                    int toSelect = Convert.ToInt32(lastestCommit.Changeset) - LastCommitId;
+
+                    var commits = result.results.Take(toSelect).ToList();
+
+                    await BroadcastAllGuilds(commits);
+                }
+
+            }
+
         }
     }
 }
